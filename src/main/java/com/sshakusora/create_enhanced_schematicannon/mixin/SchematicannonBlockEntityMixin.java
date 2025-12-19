f10df58e5f4d803d8dfc1100da1fee42dacd07c2
SShakusora <1404928002@qq.com>
2025-12-19 12:24:07 +0800

    feat:ready for create6.0。

--- src/main/java/com/sshakusora/create_enhanced_schematicannon/mixin/SchematicannonBlockEntityMixin.java
+++
@@ -1,8 +0,0 @@
-package com.sshakusora.create_enhanced_schematicannon.mixin;
-
-import com.simibubi.create.content.schematics.cannon.SchematicannonBlockEntity;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(SchematicannonBlockEntity.class)
-public class SchematicannonBlockEntityMixin {
-}
