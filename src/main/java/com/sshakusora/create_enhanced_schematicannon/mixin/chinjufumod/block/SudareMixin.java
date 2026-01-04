7ae5e99bc3b7e217d590c978e05eac2966d59804
SShakusora <1404928002@qq.com>
2026-01-04 14:24:22 +0800

    fix: void boiler

--- src/main/java/com/sshakusora/create_enhanced_schematicannon/mixin/chinjufumod/block/SudareMixin.java
+++ src/main/java/com/sshakusora/create_enhanced_schematicannon/mixin/chinjufumod/block/SudareMixin.java
@@ -1,5 +1,6 @@
 package com.sshakusora.create_enhanced_schematicannon.mixin.chinjufumod.block;
 
+import com.ayutaki.chinjufumod.blocks.garden.Sudare;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
@@ -8,18 +9,15 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
-import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Pseudo
-@Mixin(targets = "com.ayutaki.chinjufumod.blocks.garden.Sudare")
+@Mixin(Sudare.class)
 public class SudareMixin extends Block{
-    @Unique private final IntegerProperty STAGE_1_3 = IntegerProperty.create("stage", 1, 3);
     public SudareMixin(Properties p_49795_) {
         super(p_49795_);
     }
@@ -32,11 +30,11 @@ public class SudareMixin extends Block{
                 level.setBlock(above, state.setValue(BlockStateProperties.DOUBLE_BLOCK_HALF, DoubleBlockHalf.UPPER), 18);
             }
         } else if(state.getValue(BlockStateProperties.DOUBLE_BLOCK_HALF) == DoubleBlockHalf.UPPER) {
-            if (state.getValue(STAGE_1_3) == 2 || state.getValue(STAGE_1_3) == 3) level.setBlock(pos, state, 18);
+            if (state.getValue(Sudare.STAGE_1_3) == 2 || state.getValue(Sudare.STAGE_1_3) == 3) level.setBlock(pos, state, 18);
         }
     }
 
-    @Inject(method = "m_6402_", at = @At("HEAD"), cancellable = true, remap = false)
+    @Inject(method = "setPlacedBy", at = @At("HEAD"), cancellable = true)
     private void setPlacedByFix(Level worldIn, BlockPos pos, BlockState state, LivingEntity placer, ItemStack stack, CallbackInfo ci) {
         if(placer == null) ci.cancel();
     }
--- src/main/java/com/sshakusora/create_enhanced_schematicannon/mixin/create/block/BoilerHeatersFix.java
+++ src/main/java/com/sshakusora/create_enhanced_schematicannon/mixin/create/block/BoilerHeatersFix.java
@@ -1,8 +1,8 @@
 package com.sshakusora.create_enhanced_schematicannon.mixin.create.block;
 
+import com.simibubi.create.AllTags;
 import com.simibubi.create.content.fluids.tank.FluidTankBlock;
 import com.simibubi.create.content.fluids.tank.FluidTankBlockEntity;
-import com.simibubi.create.content.processing.burner.BlazeBurnerBlock;
 import com.simibubi.create.content.schematics.SchematicPrinter;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
@@ -13,11 +13,11 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(SchematicPrinter.class)
-public class BlazeBurnerBlockFix {
+public class BoilerHeatersFix {
     @Inject(method = "shouldPlaceBlock", at = @At("RETURN"), remap = false, cancellable = true)
     private void shouldPlaceBlock(Level world, SchematicPrinter.PlacementPredicate predicate, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
         BlockState toReplace = world.getBlockState(pos);
-        if(!(toReplace.getBlock() instanceof BlazeBurnerBlock)) return;
+        if (!AllTags.AllBlockTags.PASSIVE_BOILER_HEATERS.matches(toReplace)) return;
 
         BlockPos abovePos = pos.above();
         BlockState aboveState = world.getBlockState(abovePos);
@@ -27,7 +27,7 @@ public class BlazeBurnerBlockFix {
         FluidTankBlockEntity tankBE = tank.getBlockEntity(world, abovePos);
         if (tankBE != null) {
             FluidTankBlockEntity controllerBE = tankBE.getControllerBE();
-            if (controllerBE != null && controllerBE.boiler.isActive() &&isReplaced) cir.setReturnValue(false);
+            if (controllerBE != null && controllerBE.boiler.isActive() && isReplaced) cir.setReturnValue(false);
         }
     }
 }
